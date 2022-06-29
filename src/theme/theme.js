666eda3e7a01bc094bce479623a26e68a34c7f89
Alfred Warui <alfredwarui17@gmail.com>
2022-06-29 15:56:41 +0300

    create custom theme palette

---
+++ src/theme/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5f0c9c",
+      light: "#883dbd",
+      dark: "#400868",
+    },
+    secondary: {
+      main: "#737373",
+      light: "#c0c0c0",
+      dark: "#333333",
+    },
+  },
+});
