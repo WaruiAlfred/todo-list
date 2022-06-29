624da50ad22ad093be927b41d2c8b36687a63122
Alfred Warui <alfredwarui17@gmail.com>
2022-06-30 02:49:46 +0300

    uninstall react query which is unused

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Todos from "./components/Todos";
 import { theme } from "./theme/theme";
-import { QueryClientProvider, QueryClient } from "react-query";
-
-const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Todos />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Todos />
+    </ThemeProvider>
   );
 };
 
